881fe67770670e2ccd990fc94d60b01514d54c2d
franco <francodiegorioja@gmail.com>
2026-06-17 16:50:37 -0500

    feat(subscriptions): add ISubscriptionCommandService and SubscriptionCommandService

---
+++ TechnoByteLambders.MediTrackSensor.Platform/Subscriptions/Application/Internal/CommandServices/SubscriptionCommandService.cs
@@ -0,0 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using TechnoByteLambders.MediTrackSensor.Platform.Shared.Application.Patterns;
+using TechnoByteLambders.MediTrackSensor.Platform.Shared.Domain.Repositories;
+using TechnoByteLambders.MediTrackSensor.Platform.Subscriptions.Application.CommandServices;
+using TechnoByteLambders.MediTrackSensor.Platform.Subscriptions.Domain.Model.Aggregates;
+using TechnoByteLambders.MediTrackSensor.Platform.Subscriptions.Domain.Model.Commands;
+using TechnoByteLambders.MediTrackSensor.Platform.Subscriptions.Domain.Model.Errors;
+using TechnoByteLambders.MediTrackSensor.Platform.Subscriptions.Domain.Repositories;
+
+namespace TechnoByteLambders.MediTrackSensor.Platform.Subscriptions.Application.Internal.CommandServices;
+
+public class SubscriptionCommandService(
+    ISubscriptionRepository subscriptionRepository,
+    IUnitOfWork unitOfWork) : ISubscriptionCommandService
+{
+    public async Task<Result<Subscription, string>> Handle(CreateSubscriptionCommand command, CancellationToken cancellationToken = default)
+    {
+        var subscription = new Subscription(command);
+        try
+        {
+            await subscriptionRepository.AddAsync(subscription, cancellationToken);
+            await unitOfWork.CompleteAsync(cancellationToken);
+            return new Result<Subscription, string>.Success(subscription);
+        }
+        catch (OperationCanceledException) { return new Result<Subscription, string>.Failure(SubscriptionsErrors.SubscriptionCreationFailed.Description); }
+        catch (DbUpdateException) { return new Result<Subscription, string>.Failure(SubscriptionsErrors.SubscriptionCreationFailed.Description); }
+        catch (Exception) { return new Result<Subscription, string>.Failure(SubscriptionsErrors.SubscriptionCreationFailed.Description); }
+    }
+}
