db4728745e0430095fad8a156ea5e2a42b0021af
gabrielito4334 <gabriel.2montoyatorres@gmail.com>
2026-06-18 01:22:17 -0500

    feat(iam): Add User repository and IAM context configuration

---
+++ TechnoByteLambders.MediTrackSensor.Platform/Iam/Domain/Repositories/IUserRepository.cs
@@ -0,0 +1,6 @@
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Domain.Model.Aggregates;
+using TechnoByteLambders.MediTrackSensor.Platform.Shared.Domain.Repositories;
+
+namespace TechnoByteLambders.MediTrackSensor.Platform.Iam.Domain.Repositories;
+
+public interface IUserRepository : IBaseRepository<User>;
