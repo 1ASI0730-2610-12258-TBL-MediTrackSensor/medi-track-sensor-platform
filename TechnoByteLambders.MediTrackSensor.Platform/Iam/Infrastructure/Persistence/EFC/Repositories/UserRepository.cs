a637aedae9316941822cabbd65fcbd60507cb505
gabrielito4334 <gabriel.2montoyatorres@gmail.com>
2026-06-18 01:19:26 -0500

    feat(iam): Add User repository and IAM context configuration

---
+++ TechnoByteLambders.MediTrackSensor.Platform/Iam/Infrastructure/Persistence/EFC/Repositories/UserRepository.cs
@@ -0,0 +1,10 @@
+using Microsoft.EntityFrameworkCore;
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Domain.Model.Aggregates;
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Domain.Model.ValueObjects;
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Domain.Repositories;
+using TechnoByteLambders.MediTrackSensor.Platform.Shared.Infrastructure.Persistence.EFC.Configuration;
+using TechnoByteLambders.MediTrackSensor.Platform.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace TechnoByteLambders.MediTrackSensor.Platform.Iam.Infrastructure.Persistence.EFC.Repositories;
+
+public class UserRepository(AppDbContext context) : BaseRepository<User>(context), IUserRepository;
