8b9d85fd46c2c066517d7fc38e88d400db2681d9
gabrielito4334 <gabriel.2montoyatorres@gmail.com>
2026-06-18 01:23:00 -0500

    feat(iam): implement command service for the IAM BC

---
+++ TechnoByteLambders.MediTrackSensor.Platform/Iam/Application/CommandServices/IUserCommandServices.cs
@@ -0,0 +1,9 @@
+﻿using TechnoByteLambders.MediTrackSensor.Platform.Iam.Domain.Model.Aggregates;
+using TechnoByteLambders.MediTrackSensor.Platform.Shared.Application.Patterns;
+
+namespace TechnoByteLambders.MediTrackSensor.Platform.Iam.Application.CommandServices;
+
+public interface IUserCommandService
+{
+    Task<Result<bool, string>> DeleteAsync(int id, CancellationToken cancellationToken = default);
+}
---
+++ TechnoByteLambders.MediTrackSensor.Platform/Iam/Application/Internal/CommandServices/UserCommandService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Application.CommandServices;
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Application.Internal.OutboundServices;
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Domain.Model.Aggregates;
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Domain.Model.Errors;
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Domain.Repositories;
+using TechnoByteLambders.MediTrackSensor.Platform.Shared.Application.Patterns;
+using TechnoByteLambders.MediTrackSensor.Platform.Shared.Domain.Repositories;
+
+namespace TechnoByteLambders.MediTrackSensor.Platform.Iam.Application.Internal.CommandServices;
+
+public class UserCommandService(
+    IUserRepository userRepository,
+    IUnitOfWork unitOfWork,
+    IHashingService hashingService,
+    ITokenService tokenService) : IUserCommandService
+{
+
+    public async Task<Result<bool, string>> DeleteAsync(int id, CancellationToken cancellationToken = default)
+    {
+        var user = await userRepository.FindByIdAsync(id, cancellationToken);
+        if (user is null)
+            return new Result<bool, string>.Failure(IamErrors.UserNotFound.Description);
+
+        userRepository.Remove(user);
+        await unitOfWork.CompleteAsync(cancellationToken);
+        return new Result<bool, string>.Success(true);
+    }
+}
