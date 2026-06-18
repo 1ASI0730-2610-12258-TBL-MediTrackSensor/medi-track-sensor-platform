5a32b81dc951fa42f06855a8b2d833fec78847cc
gabrielito4334 <gabriel.2montoyatorres@gmail.com>
2026-06-18 00:04:27 -0500

    feat(iam): add Iam context configuration to create schemas and User repository for function management

---
+++ TechnoByteLambders.MediTrackSensor.Platform/Iam/Infrastructure/Persistence/EFC/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Domain.Model.Aggregates;
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Domain.Model.ValueObjects;
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Domain.Repositories;
+using TechnoByteLambders.MediTrackSensor.Platform.Shared.Infrastructure.Persistence.EFC.Configuration;
+using TechnoByteLambders.MediTrackSensor.Platform.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace TechnoByteLambders.MediTrackSensor.Platform.Iam.Infrastructure.Persistence.EFC.Repositories;
+
+public class UserRepository(AppDbContext context) : BaseRepository<User>(context), IUserRepository {
+    
+    public async Task<bool> ExistsByEmailAsync(string email, CancellationToken cancellationToken = default)
+    {
+        return await Context.Set<User>()
+            .AnyAsync(u => u.Email == new Email(email), cancellationToken);
+    }
+
+}
