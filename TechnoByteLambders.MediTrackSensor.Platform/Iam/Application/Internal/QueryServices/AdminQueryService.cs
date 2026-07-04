17f1094624a10a455d704037337716a26eb13451
DhilsenDevs <160343685+Dhilsen18@users.noreply.github.com>
2026-07-04 13:15:04 -0500

    feat(iam): add IAdminQueryService and AdminQueryService

---
+++ TechnoByteLambders.MediTrackSensor.Platform/Iam/Application/Internal/QueryServices/AdminQueryService.cs
@@ -0,0 +1,12 @@
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Application.QueryServices;
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Domain.Model.Aggregates;
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Domain.Model.Queries;
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Domain.Repositories;
+
+namespace TechnoByteLambders.MediTrackSensor.Platform.Iam.Application.Internal.QueryServices;
+
+public class AdminQueryService(IAdminRepository adminRepository) : IAdminQueryService
+{
+    public async Task<IEnumerable<Admin>> Handle(GetAllAdminsQuery query, CancellationToken cancellationToken = default)
+        => await adminRepository.ListAsync(cancellationToken);
+}
