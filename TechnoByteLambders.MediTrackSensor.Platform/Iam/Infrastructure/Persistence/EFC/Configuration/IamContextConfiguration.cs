ad5473a13dedf733abbc776de5f77883ab16c202
DhilsenDevs <160343685+Dhilsen18@users.noreply.github.com>
2026-07-04 13:37:33 -0500

    feat(iam): add UserRepository and EF configuration

--- TechnoByteLambders.MediTrackSensor.Platform/Iam/Infrastructure/Persistence/EFC/Configuration/IamContextConfiguration.cs
+++ TechnoByteLambders.MediTrackSensor.Platform/Iam/Infrastructure/Persistence/EFC/Configuration/IamContextConfiguration.cs
@@ -33,18 +33,5 @@ public static class IamContextConfiguration
                 .HasColumnName("email")
                 .HasMaxLength(100);
         });
-
-        builder.Entity<Admin>(entity =>
-        {
-            entity.HasKey(a => a.Id);
-            entity.Property(a => a.Id).HasColumnName("id");
-            entity.Property(a => a.EntityName).HasColumnName("entity_name").IsRequired().HasMaxLength(400);
-            entity.Property(a => a.EntityCode).HasColumnName("entity_code").IsRequired().HasMaxLength(20);
-            entity.Property(a => a.Schedule).HasColumnName("schedule").HasMaxLength(100);
-
-            entity.Property(a => a.UserId)
-                .HasConversion(v => v.Value, v => new UserId(v))
-                .HasColumnName("users_id");
-        });
     }
 }
