f30e9743ef6b1b08805669c6734823f59224c3fa
DhilsenDevs <160343685+Dhilsen18@users.noreply.github.com>
2026-07-04 13:37:33 -0500

    feat(iam): add IUserQueryService and UserQueryService

---
+++ TechnoByteLambders.MediTrackSensor.Platform/Iam/Application/Internal/QueryServices/UserQueryService.cs
@@ -0,0 +1,12 @@
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Application.QueryServices;
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Domain.Model.Aggregates;
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Domain.Model.Queries;
+using TechnoByteLambders.MediTrackSensor.Platform.Iam.Domain.Repositories;
+
+namespace TechnoByteLambders.MediTrackSensor.Platform.Iam.Application.Internal.QueryServices;
+
+public class UserQueryService(IUserRepository userRepository) : IUserQueryService
+{
+    public async Task<IEnumerable<User>> Handle(GetAllUsersQuery query, CancellationToken cancellationToken = default)
+        => await userRepository.ListAsync(cancellationToken);
+}
